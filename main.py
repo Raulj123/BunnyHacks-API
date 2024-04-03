e9bb7a1fc8c57f1f85b16e0c1768e8a391e75c67
raulj <112786129+Raulj123@users.noreply.github.com>
2024-04-03 11:41:34 -0700

    API for egg3 Hunt

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -18,3 +17,12 @@ def message():
         "message": "Oh no! It seems that watering the eggs accidentally washed the code into Local Storage! Look for a key that rhymes with 'gloom' – the value of that key is your code!",
         "secretCode": "🌊"
     }
+
+@app.get('/egg3Hunt/{lake}')
+def beeAPI(lake: str):
+    if lake == 'lake_bee':
+        return {"message": "I accidentally sent the bee (API) to the wrong lake, {lake}... It needs to be at lake_goldenEgg. Could you correct the parameter and run the API again?" }
+    elif lake == 'lake_goldenEgg':
+        return {"message": "🐝: The golden egg will reach you at time 16:24!"}
+    else:
+        return {"message": "🐝: Umm what are you on? Give me the correct params bruh 😡"}
